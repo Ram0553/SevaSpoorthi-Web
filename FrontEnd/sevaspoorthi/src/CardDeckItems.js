4da77b948301b6d31a64bf367172f2787a53f1da
daschiku37 <daschiku37@gmail.com>
2023-01-03 01:05:00 +0530

    Adding Carddeck

---
+++ FrontEnd/sevaspoorthi/src/CardDeckItems.js
@@ -0,0 +1,76 @@
+const cardDeckItems = [
+    {
+        src:"https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg",
+        alt:"Nature",
+        miniCaption:"Beautiful Place",
+        description:"acssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssacascascas",
+    },
+    {
+        src:"https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg",
+        alt:"Nature",
+        miniCaption:"Beautiful Place",
+        description:"acssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssacascascas",
+    },
+    {
+        src:"https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg",
+        alt:"Nature",
+        miniCaption:"Beautiful Place",
+        description:"acssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssacascascas",
+    },
+    {
+        src:"https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg",
+        alt:"Nature",
+        miniCaption:"Beautiful Place",
+        description:"acssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssacascascas",
+    },
+    {
+        src:"https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg",
+        alt:"Nature",
+        miniCaption:"Beautiful Place",
+        description:"acssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssacascascas",
+    },
+    {
+        src:"https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg",
+        alt:"Nature",
+        miniCaption:"Beautiful Place",
+        description:"acssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssacascascas",
+    },
+    {
+        src:"https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg",
+        alt:"Nature",
+        miniCaption:"Beautiful Place",
+        description:"acssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssacascascas",
+    },
+    {
+        src:"https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg",
+        alt:"Nature",
+        miniCaption:"Beautiful Place",
+        description:"acssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssacascascas",
+    },
+    {
+        src:"https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg",
+        alt:"Nature",
+        miniCaption:"Beautiful Place",
+        description:"acssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssacascascas",
+    },
+    {
+        src:"https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg",
+        alt:"Nature",
+        miniCaption:"Beautiful Place",
+        description:"acssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssacascascas",
+    },
+    {
+        src:"https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg",
+        alt:"Nature",
+        miniCaption:"Beautiful Place",
+        description:"acssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssacascascas",
+    },
+    {
+        src:"https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg",
+        alt:"Nature",
+        miniCaption:"Beautiful Place",
+        description:"acssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssacascascas",
+    },
+];
+
+export default cardDeckItems;
