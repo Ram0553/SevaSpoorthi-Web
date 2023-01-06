8b823f18a12e16740120a39f2bf791dbcd3084a1
daschiku37 <daschiku37@gmail.com>
2023-01-06 16:32:13 +0530

    Requirements modified

---
+++ FrontEnd/sevaspoorthi/src/Components/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import './HomePage.css';
+import ScrollNotification from '../ScrollNotification/ScrollNotification';
+function HomePage(props)
+{
+    return (
+        <ScrollNotification/>
+    );
+}
+
+export default HomePage;
