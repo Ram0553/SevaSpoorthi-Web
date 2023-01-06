8f59a9f5959b34d20c4c8c592e6158730a0effb3
daschiku37 <daschiku37@gmail.com>
2023-01-06 16:26:42 +0530

    Added active listener on ScrollNotification in fireBase

--- FrontEnd/sevaspoorthi/src/Components/ScrollNotification/ScrollNotification.js
+++ FrontEnd/sevaspoorthi/src/Components/ScrollNotification/ScrollNotification.js
@@ -1,33 +1,78 @@
 import './ScrollNotification.css';
-import { useEffect } from 'react';
+import { useState,useRef,useEffect } from 'react';
+import { fireDb } from '../../Config/Firebase';
+import { ref,onValue } from 'firebase/database';
 
-function ScrollNotification(props)
+function isEqual(stateNotificationsKeyList,newNotificationsKeyList)
 {
-    const notifications = props.notifications;
-    const notificationsDiv = notifications.map((notification,index) => {
-        return (
-            <div className="notification" key={index}>
-                <a href={notification.url}>  <strong>🆕📢{notification.content}</strong></a>
-            </div>
-        );
-    }); 
-    
-    useEffect(() => {
-        const divLength = document.getElementsByClassName("scrollElement")[0].scrollWidth;
-        const paddedDivLength = divLength+100;
-        document.getElementsByClassName("scrollNotification")[0].style.cssText = "--paddedDivLength: " + paddedDivLength.toString()+"px";
+    if(stateNotificationsKeyList.length === 0){return false;}
+    if(stateNotificationsKeyList.length !== newNotificationsKeyList.length){return false;}
+    stateNotificationsKeyList.sort();
+    newNotificationsKeyList.sort();
+    for(var key=0;key<stateNotificationsKeyList.length;key++)
+    {
+        if(stateNotificationsKeyList[key] !== newNotificationsKeyList[key]){return false;}
+    }
+    return true;
+}
+
+function ScrollNotification()
+{
+    const notificationRef = ref(fireDb, "Notification");
+    const [notificationsDiv,updateNotificationsDiv] = useState([]);
+    const notificationsList = useRef({});
+    onValue(notificationRef,(snapshot) => {
+        if(snapshot.exists())
+        {
+            const notifications = snapshot.val();
+            if(!isEqual(Object.keys(notificationsList.current),Object.keys(notifications)))
+            {
+                notificationsList.current = notifications;
+                var newNotificationsDiv = [];
+                for (var notif_key in notifications)
+                {
+                    var value = notifications[notif_key];
+                    newNotificationsDiv.push(
+                        <div className="notification" key={notif_key}>
+                            <a href={value.url}>  <strong>🆕📢{value.content}</strong></a>
+                        </div>
+                    );
+                }
+                updateNotificationsDiv(newNotificationsDiv);
+            }
+        }
+        else if(notificationsDiv.length !== 0)
+        {
+            updateNotificationsDiv([]);
+        }
     });
 
-    return (
-        <div className="scrollNotification">
-            <div className="scrollElement primary">
-                {notificationsDiv}
+    useEffect(() => {
+        if(notificationsDiv.length !== 0)
+        {
+            const divLength = document.getElementsByClassName("scrollElement")[0].scrollWidth;
+            const paddedDivLength = divLength+100;
+            document.getElementsByClassName("scrollNotification")[0].style.cssText = "--paddedDivLength: " + paddedDivLength.toString()+"px";
+        }
+    });
+
+    if(notificationsDiv.length !== 0)
+    {
+        return (
+            <div className="scrollNotification">
+                <div className="scrollElement primary">
+                    {notificationsDiv}
+                </div>
+                <div className="scrollElement secondary">
+                    {notificationsDiv}
+                </div>
             </div>
-            <div className="scrollElement secondary">
-                {notificationsDiv}
-            </div>
-        </div>
-    );
+        );    
+    }
+    else
+    {
+        return (<></>);
+    }
 }
 
 export default ScrollNotification;
