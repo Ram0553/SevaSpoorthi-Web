fcdccacdd6159a10690655ee461955cc4ed346ce
Ram0553 <shreerammohanty436@gmail.com>
2023-01-10 13:26:38 +0530

    Added admin part and backend of homepage carousel

--- FrontEnd/sevaspoorthi/src/Components/HomepageCarousel/HomepageCarousel.js
+++ FrontEnd/sevaspoorthi/src/Components/HomepageCarousel/HomepageCarousel.js
@@ -1,14 +1,36 @@
 import './HomepageCarousel.css'
 import Carousel from '../Carousel/Carousel';
 import homepage from './HomepageSlides'
+import { useEffect, useState } from 'react';
+import { child, get, ref } from 'firebase/database';
+import { fireDb } from '../../Config/Firebase';
 
 function HomepageCarousel(props)
 {
     const outerCarouselStyleSettings = {
         height:"80vh"
       };
+
+    const [slides, setSlides] = useState([]);
+
+    const fetchSlides = () => {
+        get(child(ref(fireDb),"Photos/HomepageCarousel")).then((snapshot)=>{
+            if(snapshot.exists()){
+                snapshot.forEach((photo)=>{
+                    const plink=photo.child("Link").val();
+                    setSlides(photos=>[...photos,plink]);
+                })
+            }
+        });
+        return;
+    }
+
+    useEffect(() => {
+      fetchSlides();
+    }, [])
+    
     return (
-        <Carousel className="homepage-carousel" slides={homepage.slides} styleSettings={outerCarouselStyleSettings}/>
+        <Carousel className="homepage-carousel" slides={slides} styleSettings={outerCarouselStyleSettings}/>
     );
 }
 
