38b9663257559c7b231bfe68533567887f51d791
Ram0553 <shreerammohanty436@gmail.com>
2023-01-15 19:44:57 +0530

    Added css for footer and responsiveness.

--- FrontEnd/sevaspoorthi/src/Components/Footer/Footer.js
+++ FrontEnd/sevaspoorthi/src/Components/Footer/Footer.js
@@ -8,8 +8,9 @@ import {
   } from "@fortawesome/free-brands-svg-icons";
 import logo from './logo.png';
 import mapImage from './map.png';
+import Map from '../GoogleMap/Map';
 
-function FooterSocialMediaLinks(props)
+function FooterSocialMediaLinks()
 {
     return (
         <div className = 'footer-socialmedialinks'>
@@ -35,7 +36,7 @@ function FooterSocialMediaLinks(props)
     );
 }
 
-function FooterBody(props)
+function FooterBody()
 {
     return (
         <div className="footer-body">
@@ -44,29 +45,35 @@ function FooterBody(props)
                 <img src={logo} alt="Logo" style={{ maxWidth:"50%",aspectRatio:"1/1",float:"left" }}/>
                 <p> Seva Spoorthi Foundation is a NGO working for the cause of welfare of the farming community, with Specific advisory on organic farming, related  farm technologies  to  encourage  farmers  for organized Production to make available healthy products to consumers, improve income of the farmers and Sustainable agriculture in the states of Telangana and AP. We have been creating awareness on organic, IPM and good farming practices since 2020. </p>
             </div>
-            <div className="quick-links">
-                <h3> Quick Links </h3>
-                <a href=""> <p> Home </p> </a>
-                <a href=""> <p> About </p> </a>
-                <a href=""> <p> Donate </p> </a>
-                <a href=""> <p> Villages </p> </a>
-            </div>
-            <div className="connect">
-                <h3> Connect </h3>
-                <a href=""> <p> Our Mission </p> </a>
-                <a href=""> <p> Notifications </p> </a>
-                <a href=""> <p> News </p> </a>
-                <a href=""> <p> Events </p> </a>
-            </div>
-            <div className="offices-map">
-                <div>
-                    <h3> Contact Us </h3>
-                    <p> Hyd Office Location</p>
-                    <p> Call Us: phonenumber </p>
+            <div className='footer-body-inner'>
+                <div className="quick-links">
+                    <h3> Quick Links </h3>
+                    <div className='quick-links-inner'>
+                        <a href="/"> <p> Home </p> </a>
+                        <a href="/AboutUs"> <p> About </p> </a>
+                        <a href=""> <p> Donate </p> </a>
+                        <a href=""> <p> Villages </p> </a>
+                    </div>
                 </div>
-                <div className="google-map">
-                    <img src={mapImage} alt="Map" style={{width:"95%",aspectRatio:"1/1"}}/>
-                </div>                
+                <div className="connect">
+                    <h3> Connect </h3>
+                    <div className='connect-inner'>
+                        <a href=""> <p> News </p> </a>
+                        <a href=""> <p> Programs </p> </a>
+                        <a href=""> <p> Gallery </p> </a>
+                        <a href=""> <p> Notifications </p> </a>
+                    </div>
+                </div>
+                <div className="offices-map">
+                    <div>
+                        <h3> Contact Us </h3>
+                        <div className='contact'>
+                            <a href="tel:+919994241399">Call Us!</a>
+                            <a href="mailto:sevaspoorthi.web@gmail.com">Send Email</a>
+                        </div>
+                    </div>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24456.418063578833!2d78.52842894260648!3d17.347236081597234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb98812f186801%3A0x20635e36d1b259d9!2sAnjireddynagar%20Colony%2C%20Sai%20Ram%20Nagar%20Colony%2C%20Champapet%2C%20Telangana%20500079!5e0!3m2!1sen!2sin!4v1673719071094!5m2!1sen!2sin" zoom="16" className='iframe-map'  allowFullScreen={true} loading="lazy"></iframe>               
+                </div>
             </div>
         </div>
     );
@@ -76,12 +83,12 @@ function FooterCopyright(props)
 {
     return (
         <div className="footer-copyright">
-            &copy;Created by Sevapoorthi 💖💖💖
+            &copy;Created by SevaSpoorthi Tech Team
         </div>
     );
 }
 
-function Footer(props)
+function Footer()
 {
     return (
         <div className="footer">
