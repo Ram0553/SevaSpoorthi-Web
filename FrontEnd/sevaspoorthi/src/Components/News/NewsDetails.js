15c013378daba6a2585c1d9167c7a5bbd6c2660d
Ram0553 <shreerammohanty436@gmail.com>
2023-03-06 11:39:57 +0530

    downloa option added

--- FrontEnd/sevaspoorthi/src/Components/News/NewsDetails.js
+++ FrontEnd/sevaspoorthi/src/Components/News/NewsDetails.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Carousel from "../Carousel/Carousel";
 import NavBar from "../NavBar/NavBar";
+import './NewsDetails.css';
+import {saveAs} from 'file-saver'
 
 function NewsDetails(){
     const location = useLocation();
@@ -9,15 +11,25 @@ function NewsDetails(){
       document.getElementsByClassName("news")[0].innerHTML=location.state.news["content"];
     }, []);
     
+    function download(){
+        var images = location.state.news["images"];
+        console.log(images.length);
+        for(var i = 0; i<images.length; i++){
+            saveAs(images[i],"image"+i.toString()+".jpg");
+        }
+    }
+
     return (
         <div>
             <NavBar/>
             {location.state.news["images"].length!=0?<Carousel className="homepage-carousel" slides={location.state.news["images"]}/>:""}
             <h1 style={{margin:"2vw",textAlign: "center",}}>{location.state.news["heading"]}</h1>
-            <div className="news">
+            <div className="news"/>
+            <div className="download">
+                {location.state.news["images"].length!=0?<button onClick={download}>Download Images</button>:""}
             </div>
         </div>
-    )
+    );
 }
 
 export default NewsDetails;
